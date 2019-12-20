71da713826f92fd8d360097216a9ce724f4b0c48
bruno edoh <edohbruno@gmail.com>
2019-12-20 11:59:05 +0000

    playing with functions

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ func square(a int) int {
   return a * a
 }
 
+func something_and_two(w string) (string, int) {
+  return w, 2
+}
+
 func main() {
   fmt.Println("Hello World")
   fmt.Println(square(100))
+  fmt.Println(something_and_two("Go home today for Christmas"))
 }
